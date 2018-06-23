fb6c2e517d22a02207622fc247fcbdbbddcb2e03
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-23 19:34:52 +0530

    modified:   public/js/index.js 	modified:   server/server.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io()
 
 socket.on('connect', () => {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    from: 'Shubham',
-    text: 'Hello World!'
-  })
 })
 
 socket.on('disconnect', () => {
