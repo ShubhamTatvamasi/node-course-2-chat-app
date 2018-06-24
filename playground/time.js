c965af9b46cb19a90a4751f2bfaa1791e6d1d496
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-24 19:43:12 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   playground/time.js

--- playground/time.js
+++ playground/time.js
@@ -1,3 +1,9 @@
+const moment = require('moment')
 
-const date = new Date()
-console.log(date)
+// const date = new Date()
+// const months = ['Jan', 'Feb', 'Mar', 'Apr']
+
+const date = moment()
+// date.add(100, 'years').subtract(9, 'months')
+// console.log(date.format('MMM Do, YYYY'))
+console.log(date.format('h:mm a'))
